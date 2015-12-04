49699d4b1c9a84ab26f1db5454b787b6f6666e9e
tbone10 <tmorgan1010@outlook.com>
2015-12-03 20:53:44 -0500

    Implemented product list page

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,41 @@ growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly",
                 <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
                 </p>
                 <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
+
+
+copywriting_monthly = Product.create(title: "Copywriting Monthly", 
+    subtitle: "Fart Binders", author: "John Morgan", 
+    length: "6 months", author_image_name: "teacher-image.jpg", 
+    price: "4.99", sku: "COPYWRITE2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+    details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+
+            <p>In this series, Mattan Griffel introduces you to the concept of copywriting and Fart Binders and shares his favorite tips for getting started as a growth hacker.</p>
+            <p><strong>What You'll Learn</strong></p>
+            <ul class="no-indent">
+                <li>How to write killer copy</li>
+                <li>Seven secrets of direct copy ad sales</li>
+            </ul>}, 
+    author_description: %{                <p>Hey, I'm John Morgan, co-founder of JMA Packaging Solutions <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
+
+
+analytics_monthly = Product.create(title: "Analytics Monthly", 
+    subtitle: "Tits Stick", author: "Tim Morgan", 
+    length: "6 months", author_image_name: "teacher-image.jpg", 
+    price: "4.99", sku: "ANALYTICS2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+    details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+
+            <p>In this series, Tim Morgan introduces you to the concept of masterbating and shares his favorite tips for getting off to hot black chicks.</p>
+            <p><strong>Will you Cum?</strong></p>
+            <ul class="no-indent">
+                <li>How to use mix panel effectively</li>
+                <li>How to improv your sales funnel using analytics</li>
+            </ul>}, 
+    author_description: %{                <p>Tim Morgan, co-founder of JMA Packaging Solutions <a href="http://www.onemonth.com" target="_blank">One Month</a> willteach the best selling Jerk-Off tactics.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
